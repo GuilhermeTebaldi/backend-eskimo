b8a7c15ba8e4a5839a7f5f50089b4078c6600d73
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-04-25 09:38:50 -0300

    feat: adicionar POST, PUT e DELETE em SubcategoriesController

--- Controllers/SubcategoriesController.cs
+++ Controllers/SubcategoriesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using e_commerce.Data;
@@ -17,10 +18,47 @@ namespace e_commerce.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> GetAll()
         {
             var subcategories = await _context.Subcategories.ToListAsync();
             return Ok(subcategories);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create([FromBody] Subcategory subcategory)
+        {
+            _context.Subcategories.Add(subcategory);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAll), new { id = subcategory.Id }, subcategory);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Update(int id, [FromBody] Subcategory updated)
+        {
+            var subcategory = await _context.Subcategories.FindAsync(id);
+            if (subcategory == null)
+                return NotFound();
+
+            subcategory.Name = updated.Name;
+            subcategory.CategoryId = updated.CategoryId;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var subcategory = await _context.Subcategories.FindAsync(id);
+            if (subcategory == null)
+                return NotFound();
+
+            _context.Subcategories.Remove(subcategory);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
