837f6246f896d0f6e7bac086f694624d8ad410e5
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-04-30 14:13:01 -0300

    fix: adicionar model Setting ao projeto

---
+++ Models/Setting.cs
@@ -0,0 +1,10 @@
+namespace e_commerce.Models
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+        public double DeliveryRate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
