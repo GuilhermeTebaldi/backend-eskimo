7d02d8dd45495795eab7ebf9512f66af513989f8
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-07-06 17:54:28 +0200

    “Dd”

--- DTOs/OrderItemDTO.cs
+++ DTOs/OrderItemDTO.cs
@@ -3,10 +3,10 @@ namespace CSharpAssistant.API.DTOs
     public class OrderItemDTO
     {
         public int ProductId { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
 
-        public string ProductName { get; set; }
+       // public string? ProductName { get; set; }
 
         public int Quantity { get; set; }
 
