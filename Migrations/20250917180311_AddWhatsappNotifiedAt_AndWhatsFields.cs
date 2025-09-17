6666a2fe8c9331115d1cf7d635f6a359ab5ffb48
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-09-17 20:03:12 +0200

    EF: AddWhatsappNotifiedAt_AndWhatsFields

---
+++ Migrations/20250917180311_AddWhatsappNotifiedAt_AndWhatsFields.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWhatsappNotifiedAt_AndWhatsFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WhatsappAccessToken",
+                table: "PaymentConfigs",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WhatsappPhoneNumberId",
+                table: "PaymentConfigs",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WhatsappStoreNumber",
+                table: "PaymentConfigs",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "WhatsappNotifiedAt",
+                table: "Orders",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WhatsappAccessToken",
+                table: "PaymentConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "WhatsappPhoneNumberId",
+                table: "PaymentConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "WhatsappStoreNumber",
+                table: "PaymentConfigs");
+
+            migrationBuilder.DropColumn(
+                name: "WhatsappNotifiedAt",
+                table: "Orders");
+        }
+    }
+}
--- obj/Debug/net8.0/e-commerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/e-commerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("e-commerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+715eae917ebbb60a6b3501f44f0763718e740f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a291d5d195235f6bca8cc88db0e4106f0b1cc66")]
 [assembly: System.Reflection.AssemblyProductAttribute("e-commerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("e-commerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
