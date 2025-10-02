6ef3761f0c9964df67fe737f11de2aeaedea2f6e
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-10-02 18:27:42 +0200

    (  2 aditor de ordem e estilo  )

---
+++ Migrations/20251002162639_AddProductSortAndPin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductSortAndPin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PinnedTop",
+                table: "Products",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SortRank",
+                table: "Products",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PinnedTop",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SortRank",
+                table: "Products");
+        }
+    }
+}
