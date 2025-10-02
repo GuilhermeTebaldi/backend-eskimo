5baa9076c2f6507012dcbe6c2be88e913e6c5360
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-10-02 17:57:29 +0200

    Move migrations to CSharpAssistant.API/Migrations and enable storefront layout ordering

---
+++ Migrations/20251002154447_AddProductLayout.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductLayout : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_StoreProductVisibilities_ProductId",
+                table: "StoreProductVisibilities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreProductVisibilities_ProductId",
+                table: "StoreProductVisibilities",
+                column: "ProductId");
+        }
+    }
+}
