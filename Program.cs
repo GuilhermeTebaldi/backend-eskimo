c9f7f6588932144528a573b16cea5b7ee6e12b6c
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-10-12 22:34:59 +0200

    (  ajuste   )

--- Program.cs
+++ Program.cs
@@ -36,7 +36,12 @@ using Microsoft.AspNetCore.Authentication.JwtBearer; using System.Text;
  };
  });
 
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdmin", p => p.RequireRole("admin"));
+    options.AddPolicy("RequireOperatorOrAdmin", p => p.RequireRole("admin", "operator"));
+});
+
 
 
  builder.Services.AddScoped<TokenService>();
