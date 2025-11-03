dd3911a4c7f9e43a96b8328f01f41f20c46d8410
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-11-03 16:18:52 +0100

    ( local 5174  )

--- Program.cs
+++ Program.cs
@@ -79,6 +79,7 @@ builder.Services.AddControllers()
  "http://localhost:5173",
  "http://127.0.0.1:5173",
  "https://localhost:5173",
+ "http://localhost:5174",
  "https://127.0.0.1:5173",
  
  // Admin / Site públicos
