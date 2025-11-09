78a2de0b28566563599521f677d840a46ff5bd63
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2025-11-09 22:08:36 +0100

    (   vai   )

---
+++ Migrations/20251109210730_SyncModel_20251109.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModel_20251109 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
