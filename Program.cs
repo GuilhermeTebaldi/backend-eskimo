088fbb2e7dc2c36183c31c2290b70ed63c527ded
GuilhermeTebaldi <tebaldiguilherme.roma@gmail.com>
2026-01-14 23:06:10 +0100

    ( erro de pedido   )

--- Program.cs
+++ Program.cs
@@ -185,7 +185,7 @@ app.Use(async (ctx, next) =>
             ctx.Response.Headers["Vary"] = "Origin";
             ctx.Response.Headers["Access-Control-Allow-Credentials"] = "true";
             ctx.Response.Headers["Access-Control-Allow-Headers"] = "Authorization,Content-Type,X-Store";
-            ctx.Response.Headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS";
+            ctx.Response.Headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,PATCH,DELETE,OPTIONS";
 
             if (HttpMethods.IsOptions(ctx.Request.Method))
             {
